32fbd50157f0888f474fb160a5d7ee7b95af0d63
pNagi <nagisama.j@gmail.com>
2018-08-25 21:44:37 +0700

    Added mine reward

--- bin/mine-block.js
+++ bin/mine-block.js
@@ -3,36 +3,60 @@ const { execSync } = require('child_process')
 
 const chainPath = Argv['path']
 
+const log = console.log
+
+const REWARD = 1000000
+
 function run() {
   try {
     const chainGit = `git -C ${chainPath}`
     execSync(`${chainGit} pull`)
+    execSync(`${chainGit} reset --hard origin/master`)
     execSync('export GPG_TTY=$(tty)')
 
-    const blockNumber = parseInt(
-      execSync(`${chainGit} log -1 --pretty=%B`)
-        .toString()
-        .replace(/block (\d+) \[nonce=\d+]/g, '$1')
-    )
+    const blockNumber =
+      parseInt(
+        execSync(`${chainGit} log -1 --pretty=%B`)
+          .toString()
+          .replace(/block (\d+) \[nonce=\d+]/g, '$1')
+      ) + 1
 
-    // TODO: GET SOME MONEY!
-    // Commit nonce=0
+    if (blockNumber === NaN) return
+
+    function generateCommitMessage(nonce) {
+      return `block ${blockNumber} [nonce=${nonce}]`
+    }
+
+    const accountPath = execSync(`${chainGit} config user.signingkey`)
+      .toString()
+      .match(/.{1,4}/g)
+      .join('/')
+    // TODO: Fix the case where account directory is not created yet
+    const balancePath = `${chainPath}/accounts/${accountPath}/balance`
+    const balance = parseInt(execSync(`cat ${balancePath}`).toString())
+
+    if (balance === NaN) return
+
+    execSync(`echo ${balance + REWARD} > ${balancePath}`)
+    execSync(`${chainGit} add .`)
+    execSync(`${chainGit} commit -S -m '${generateCommitMessage(0)}'`)
 
     let i = 1
     while (true) {
-      const commitMessage = `block ${blockNumber} [nonce=${i++}]`
+      const commitMessage = generateCommitMessage(i++)
       execSync(`${chainGit} commit --amend -S -m '${commitMessage}'`)
       const commitHash = execSync(`${chainGit} rev-parse HEAD`).toString()
-      console.log('commitHash', commitHash)
+      log(`${commitMessage}: ${commitHash}`)
 
       if (commitHash.substring(0, 2) === '55') {
         break
       }
     }
 
+    log('create new block!')
     execSync(`${chainGit} push`)
   } catch (error) {
-    console.log(error)
+    log(error)
     throw error
   }
 }
